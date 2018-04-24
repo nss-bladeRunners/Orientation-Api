6feeb000d257ed03f10406856ff747b2c3634e46
Jess <jessibcoden@gmail.com>
2018-04-23 21:18:28 -0500

    added Computer model & repo and added GetAll method to repo

---
+++ OrientationAPI/Models/Computer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrientationAPI.Models
+{
+    public class Computer
+    {
+        public int ComputerId {get; set;}
+        public string Manufacturer {get; set;}
+        public string Make {get; set;}
+        public DateTime Date {get; set;}
+    }
+}
