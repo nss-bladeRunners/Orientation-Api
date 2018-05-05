d5d6afea110efe18bffddd22fa823747fcd2a736
Jess <jessibcoden@gmail.com>
2018-05-05 11:30:00 -0500

    adjusted styling

--- OrientationAPI/App/Controllers/ComputerViewController.js
+++ OrientationAPI/App/Controllers/ComputerViewController.js
@@ -26,9 +26,10 @@
             $location.url(`Computers`);
         };
 
-        const computerDetailView = () => {
-            $location.url(`/Computers/Detail/${computer.ComputerId}`);
-        };
+
+        $scope.computerDetailView = function (computerId) {
+            $location.path(`/Computers/Detail/${computerId}`);
+        }
 
         $scope.computerDetail = {};
 
